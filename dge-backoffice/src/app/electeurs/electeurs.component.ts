53fa140a96e9981092323cab35a7f1560f01b05b
thesombrecoder18 <elimaneka19@gmail.com>
2025-02-19 01:46:28 +0000

    vue

--- dge-backoffice/src/app/electeurs/electeurs.component.ts
+++ dge-backoffice/src/app/electeurs/electeurs.component.ts
@@ -13,9 +13,14 @@ import {MatIcon} from '@angular/material/icon';
   styleUrls: ['./electeurs.component.scss']
 })
 export class ElecteursComponent {
+  totalElecteurs = 5000;
+  electeursAvecErreurs = 120;
+  electeursCorriges = 80;
+
   constructor(private router: Router) {}
 
   naviguer(route: string) {
     this.router.navigate([`/electeurs/${route}`]);
   }
+
 }
